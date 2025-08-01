5d35360f6c4f4b78ce7a2f004f784ac771d9f6d6
Tshiamo-ctrl <tshiamomasuluke@gmail.com>
2025-08-01 21:31:29 +0200

    Initial commit: WePool project for deployment

--- wepool_project/settings_prod.py
+++ wepool_project/settings_prod.py
@@ -2,5 +2,9 @@
 # OR if settings.py is in root:
 nano settings_prod.py
 # OR if settings.py is in root:
+nano settings_prod.py
+# OR if settings.py is in root:
+nano settings_prod.py
+# OR if settings.py is in root:
 nano settings_prod.py# OR if settings.py is in root:
 nano settings_prod.py
